8b801926bb74b9773a1a9fe83ca9f59732c968e3
Bedford <83286384+Kim-minseok123@users.noreply.github.com>
2025-06-13 15:25:23 +0900

    feat : SFX 효과 추가 및 사운드 매니저 추가

---
+++ Source/ProjectRAS/Animation/Notify/AnimNotify_PlayAudio.cpp
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Animation/Notify/AnimNotify_PlayAudio.h"
+#include "Audio/RASAudioSubsystem.h"
+
+UAnimNotify_PlayAudio::UAnimNotify_PlayAudio()
+{
+
+}
+
+void UAnimNotify_PlayAudio::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation)
+{
+	Super::Notify(MeshComp, Animation);
+	if(!MeshComp) return;
+
+	UWorld* World = MeshComp->GetWorld();
+	if (!World) return;
+
+	UGameInstance* GameInstance = World->GetGameInstance();
+	if (!GameInstance) return;
+
+	URASAudioSubsystem* AudioSubsystem = GameInstance->GetSubsystem<URASAudioSubsystem>();
+	if (AudioSubsystem)
+	{
+		AudioSubsystem->PlaySFX(AudioName, MeshComp->GetComponentLocation());
+	}
+}
---
+++ Source/ProjectRAS/Animation/Notify/AnimNotify_PlayAudio.h
@@ -0,0 +1,25 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotify.h"
+#include "AnimNotify_PlayAudio.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTRAS_API UAnimNotify_PlayAudio : public UAnimNotify
+{
+	GENERATED_BODY()
+public:
+	UAnimNotify_PlayAudio();
+
+	virtual void Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation) override;
+
+
+protected:
+	UPROPERTY(EditAnywhere)
+	FName AudioName;
+};
